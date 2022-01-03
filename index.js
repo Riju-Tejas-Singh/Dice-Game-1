5f5375cd6dfafd847ce90cc5129347e958fde2a3
Riju-Tejas-Singh <90803307+Riju-Tejas-Singh@users.noreply.github.com>
2022-01-04 05:00:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var randomNumber1=Math.random()*6;
 randomNumber1=Math.floor(randomNumber1) + 1;
 var randomNumber2=Math.random()*6;
 randomNumber2=Math.floor(randomNumber2) + 1;
-document.querySelectorAll("img")[0].setAttribute("src","images/dice" + randomNumber1 + ".png");
-document.querySelectorAll("img")[1].setAttribute("src","images/dice" + randomNumber2 + ".png");
+document.querySelectorAll("img")[0].setAttribute("src","dice" + randomNumber1 + ".png");
+document.querySelectorAll("img")[1].setAttribute("src","dice" + randomNumber2 + ".png");
 if(randomNumber1==randomNumber2){
   document.querySelector("h1").innerHTML="Draw!";
 }else if(randomNumber1>randomNumber2){
